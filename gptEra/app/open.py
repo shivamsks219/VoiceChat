a3fee1a16264764f0f929d801bd5f0479596ba11
dkssundram <110852589+dkssundram@users.noreply.github.com>
2023-04-04 18:22:16 +0530

    Update open.py

--- gptEra/app/open.py
+++ gptEra/app/open.py
@@ -1,7 +1,7 @@
 from urllib import response
 import openai
 
-openai.api_key = "Open AI key"
+openai.api_key = "OPENAI_KEY"
 
 def ans(prompt):
     response = openai.Completion.create(engine="text-davinci-003", prompt = prompt, max_tokens=200)
